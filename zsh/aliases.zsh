27e4f1b1f0076186949cde278b36ade70881c701
Christopher Bley <chrisbley@gmail.com>
2013-11-17 09:46:02 -0500

    Modify tmux config and aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,8 +3,9 @@ alias tmux="TERM=screen-256color-bce tmux"
 alias myip="curl ifconfig.me"
 alias s="screen"
 alias t="tmux"
-alias tt="tmux attach -t"
-alias tl="if tmux has; then tmux attach; else tmux new; fi"
+alias tma='tmux attach -d -t'
+alias tt='tma $(basename $(pwd))'
+alias tl='tmux new -s $(basename $(pwd))'
 alias c="clear"
 alias g="git"
 alias gsu="git submodule init && git submodule update"
